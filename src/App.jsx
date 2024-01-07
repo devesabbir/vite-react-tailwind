37d653001ebdb85feb6930141e8831dca96d728e
devesabbir <heartedsabbirdhrubo@gmail.com>
2024-01-08 01:30:11 +0600

    vite-react-tailwindcss project scafolding template

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="h-[100vh] flex justify-center items-center">
+        <p className="text-center text-3xl text-red-600">Project</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
